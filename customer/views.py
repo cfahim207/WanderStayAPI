e10e57611dd756258f6b3f772c01fbade974ecbb
cfahim880 <cfahim880@gmail.com>
2024-10-10 20:57:56 +0600

    changes done

--- customer/views.py
+++ customer/views.py
@@ -51,7 +51,7 @@ class UserRegistrationView(APIView):
             user=serializer.save()
             token=default_token_generator.make_token(user)
             uid=urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link=f'http://127.0.0.1:8000/customer/active/{uid}/{token}'
+            confirm_link=f'https://wander-stay-api.vercel.app/customer/active/{uid}/{token}'
             email_subject="Confirm Your Email"
             email_body=render_to_string("confirm_email.html",{'confirm_link': confirm_link})
             email=EmailMultiAlternatives(email_subject,'',to=[user.email])
@@ -71,7 +71,7 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active=True
         user.save()
-        return redirect("login")
+        return redirect("https://cfahim207.github.io/WanderStay-Frontend/login.html")
     else:
         return redirect('register')
     
